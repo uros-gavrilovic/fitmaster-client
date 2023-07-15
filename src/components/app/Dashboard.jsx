4cdbdf97fb1a5cb27021e764f37936b99dede5fa
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-07-15 13:46:44 +0200

    added new components

---
+++ src/components/app/Dashboard.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+const Dashboard = (props) => {
+  return (
+    <Fragment>
+      <h2>hello hello</h2>
+      <h1>Hello World!</h1>
+    </Fragment>
+  );
+};
+
+export default Dashboard;
