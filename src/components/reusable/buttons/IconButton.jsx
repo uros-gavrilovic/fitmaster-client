beee39327c52af3c79e20c18c1af45857fc06814
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-07-15 14:09:46 +0200

    removed unused imports

--- src/components/reusable/buttons/IconButton.jsx
+++ src/components/reusable/buttons/IconButton.jsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
 
 export default function IconLabelButtons(props) {
