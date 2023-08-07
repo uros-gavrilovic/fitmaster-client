bcd01a8e8076f56e94ec4852d83f93c1e1f8f223
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-08-07 12:15:53 +0200

    implemented slide for login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -7,17 +7,19 @@ import theme from "../../utils/ThemeMUI";
 import LoginIcon from "@mui/icons-material/Login";
 import HowToRegIcon from "@mui/icons-material/HowToReg";
 import FitnessCenterIcon from "@mui/icons-material/FitnessCenter";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as userActions from "../../actions/user";
 import { useNavigate } from "react-router-dom";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import HttpsRoundedIcon from "@mui/icons-material/HttpsRounded";
 import { validateField } from "../../utils/utilFunctions";
+import { Box, FormControlLabel, Slide, TextField } from "@mui/material";
 
 const Login = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [openLoginContainer, setOpenLoginContainer] = useState(true);
   const { token } = useSelector((state) => state.userReducer);
   const [loginState, setLoginState] = useState({
     username: "",
@@ -28,8 +30,8 @@ const Login = () => {
     password: false,
   });
 
-  const handleRegister = () => {
-    console.log("Register successful!");
+  const handleTabChange = () => {
+    setOpenLoginContainer((prev) => !prev);
   };
   const handleLogIn = () => {
     const hasUsernameError = validateField(
@@ -51,6 +53,9 @@ const Login = () => {
       })
     );
   };
+  const handleRegister = () => {
+    console.log("Register");
+  };
 
   useEffect(() => {
     if (token !== undefined) {
@@ -61,71 +66,113 @@ const Login = () => {
   return (
     <ThemeProvider theme={theme}>
       <ParticleBackground />
-      <CustomContainer id="test" className="vertical-center container">
-        <div style={{ color: "white", display: "inline" }}>
-          <center>
-            <h1>
-              Fit<i>Master</i>
-              <sub className="subscript">v0.1</sub>
-            </h1>
-            <FitnessCenterIcon sx={{ fontSize: "10vh" }} />
-          </center>
-        </div>
-        <IconTextField
-          id="username"
-          title="Username"
-          icon={
-            <AccountCircle style={{ color: "white" }} sx={{ mr: 1, my: 0.5 }} />
-          }
-          fullWidth
-          required
-          error={errorState.username}
-          onChange={(e) =>
-            setLoginState({ ...loginState, [e.target.id]: e.target.value })
-          }
-        />
-        <IconTextField
-          id="password"
-          title="Password"
-          type="password"
-          icon={
-            <HttpsRoundedIcon
-              style={{ color: "white" }}
-              sx={{ mr: 1, my: 0.5 }}
-            />
-          }
-          margin="dense"
-          fullWidth
-          required
-          error={errorState.password}
-          onChange={(e) =>
-            setLoginState({ ...loginState, [e.target.id]: e.target.value })
-          }
-        />
-        <div
-          style={{
-            marginTop: "5vh",
-            display: "grid",
-            width: "auto",
-            gridTemplateColumns: "1fr 1fr",
-          }}
-        >
+
+      <Slide
+        direction="left"
+        in={!openLoginContainer}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Fragment id="login-container" className="vertical-center container">
+          This is register
           <IconButton
-            title="Register"
+            title="Register new account"
             leftIcon={<HowToRegIcon style={{ color: "white" }} />}
-            variant="outlined"
+            variant="contained"
             width="100%"
             onClick={handleRegister}
           />
           <IconButton
             title="Login"
             rightIcon={<LoginIcon style={{ color: "white" }} />}
-            variant="contained"
+            variant="outlined"
             width="100%"
-            onClick={handleLogIn}
+            onClick={handleTabChange}
           />
-        </div>
-      </CustomContainer>
+        </Fragment>
+      </Slide>
+
+      {/* ---------------------------------------------------------------------------------------- */}
+
+      <Slide
+        direction="left"
+        in={openLoginContainer}
+        mountOnEnter
+        unmountOnExit
+      >
+        <Fragment id="login-container" className="vertical-center container">
+          <div style={{ color: "white", display: "inline" }}>
+            <center>
+              <h1>
+                Fit<i>Master</i>
+                <sub className="subscript">v0.1</sub>
+              </h1>
+              <FitnessCenterIcon sx={{ fontSize: "10vh" }} />
+            </center>
+          </div>
+
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <AccountCircle
+              style={{ color: "white" }}
+              sx={{ color: "action.active", mr: 1, my: 0.5 }}
+            />
+            <TextField
+              required
+              fullWidth
+              id="username"
+              label="Username"
+              variant="standard"
+              margin="dense"
+              error={errorState.username}
+              onChange={(e) =>
+                setLoginState({ ...loginState, [e.target.id]: e.target.value })
+              }
+            />
+          </Box>
+          <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+            <HttpsRoundedIcon
+              style={{ color: "white" }}
+              sx={{ mr: 1, my: 0.5 }}
+            />
+            <TextField
+              required
+              fullWidth
+              id="password"
+              label="Password"
+              margin="dense"
+              variant="standard"
+              error={errorState.password}
+              onChange={(e) =>
+                setLoginState({ ...loginState, [e.target.id]: e.target.value })
+              }
+            />
+          </Box>
+
+          <div
+            style={{
+              marginTop: "5vh",
+              display: "grid",
+              width: "auto",
+              gridTemplateColumns: "1fr 1fr",
+            }}
+          >
+            <IconButton
+              title="Register"
+              leftIcon={<HowToRegIcon style={{ color: "white" }} />}
+              variant="outlined"
+              width="100%"
+              onClick={handleTabChange}
+            />
+            <IconButton
+              title="Login"
+              rightIcon={<LoginIcon style={{ color: "white" }} />}
+              variant="contained"
+              width="100%"
+              onClick={handleLogIn}
+            />
+          </div>
+        </Fragment>
+      </Slide>
     </ThemeProvider>
   );
 };
