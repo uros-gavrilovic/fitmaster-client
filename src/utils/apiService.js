8275b21389b930e4e9a256a62ee0594e70395961
Uros Gavrilovic <urosukigavrilovic@gmail.com>
2023-10-17 00:42:25 +0200

    Updated server domain.

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import store from "../reducers/index";
 
 let serverDomain;
-serverDomain =
-  "https://fitmaster-server-fitmaster-server.azuremicroservices.io"; // Production server domain (Azure)
+serverDomain = "https://fitmaster-server.azuremicroservices.io"; // Production server domain (Azure)
 serverDomain = serverDomain || "http://localhost:3000";
 
 const handleErrors = async (error) => {
